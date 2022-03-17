5602a1d5d10cdb8a7fb61b443c11b67f1f2d5cb3
nikhil-babu0707 <87303388+nikhil-babu0707@users.noreply.github.com>
2022-03-17 17:45:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 song = "";
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
-rightWristYs = 0;
+rightWristY = 0;
 
 function preload() {
     song = loadSound("music.mp3");
@@ -25,7 +26,8 @@ function gotposes(results) {
         console.log(results);
 
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("right wrist score = " + scoreRightWrist);
         console.log("left wrist score = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -45,6 +47,30 @@ function draw() {
 
     fill("#FF0000");
     stroke("#0017AF");
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        if (rightWristY > 0 && rightWristY <= 100) {
+            document.getElementById("speed").innerHTML = "speed = 0.5×";
+            song.rate(0.5);
+        }
+        if (rightWristY > 100 && rightWristY <= 200) {
+            document.getElementById("speed").innerHTML = "speed = 1×";
+            song.rate(1);
+        }
+        if (rightWristY > 200 && rightWristY <= 300) {
+            document.getElementById("speed").innerHTML = "speed = 1.5×";
+            song.rate(1.5);
+        }
+        if (rightWristY > 300 && rightWristY <= 400) {
+            document.getElementById("speed").innerHTML = "speed = 2×";
+            song.rate(2);
+        }
+        if (rightWristY > 400 && rightWristY <= 500) {
+            document.getElementById("speed").innerHTML = "speed = 2.5×";
+            song.rate(2.5);
+        }
+
+    }
     if (scoreLeftWrist > 0.2) {
         circle(leftWristX, leftWristY, 20);
         inNumLeftWrstY = Number(leftWristY);
@@ -59,4 +85,8 @@ function play() {
     song.play();
     song.setVolume(1);
     song.rate(1);
+}
+
+function Stop() {
+    song.stop();
 }
